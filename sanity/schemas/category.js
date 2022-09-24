c58c33d72cffd02f21608f5e170018353c153ff9
Ryan Cahela <6194542+RyanCahela@users.noreply.github.com>
2022-09-23 19:54:55 -0700

    created initial sanity schema

--- sanity/schemas/category.js
+++ sanity/schemas/category.js
@@ -1,17 +1,19 @@
 export default {
-  name: 'category',
-  title: 'Category',
-  type: 'document',
+  name: "category",
+  title: "Menu Category",
+  type: "document",
   fields: [
     {
-      name: 'title',
-      title: 'Title',
-      type: 'string',
+      name: "name",
+      type: "string",
+      title: "Category Name",
+      validation: (Rule) => Rule.required(),
     },
     {
-      name: 'description',
-      title: 'Description',
-      type: 'text',
+      name: "image",
+      type: "image",
+      title: "Image of category",
+      validation: (Rule) => Rule.required(),
     },
   ],
-}
+};
